a366e23a65f1d51aa57f2b84135d979b2e7c6081
entourloupeschool <theo.dwk@free.fr>
2026-01-26 20:55:11 +0100

    feat: add ModeRamassage command for artifact collection and implement GornetixAutoBlueGoal and TestsXScale opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/opmodes/tests/TestsXScale.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode.dinitech.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.AUTO_ROBOT_CONSTRAINTS;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.BLUE_GOAL_POSE;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.CLOSE_SHOOT_BLUE_POSE;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.FIRST_ROW_BLUE_POSE;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.LINEAR_HEADING_INTERPOLATION_END_TIME;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.OBELISK_BLUE_POSE;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.X_END_ROW_BLUE;
+
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.ParallelRaceGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.pedropathing.geometry.BezierLine;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.chargeur.MaxPowerChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.chargeur.StopChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.drivePedro.FollowPath;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.shooter.InstantRangeVisionShooter;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.shooter.StopShooter;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.shooter.VisionShooter;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.trieur.MoulinCalibrate;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.trieur.MoulinCalibrationSequence;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.trieur.MoulinNextNext;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.trieur.ReadyMotif;
+import org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.vision.ContinuousUpdatesAprilTagsDetections;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.groups.ShootRevolution;
+import org.firstinspires.ftc.teamcode.dinitech.commands.modes.ModeRamassage;
+import org.firstinspires.ftc.teamcode.dinitech.opmodes.DinitechRobotBase;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ChargeurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.GamepadSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.TrieurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
+
+@Autonomous(name = "GornetixAutoBlueGoal - Dinitech", group = "Auto")
+public class GornetixAutoBlueGoal extends DinitechRobotBase {
+    private TrieurSubsystem trieurSubsystem;
+    private VisionSubsystem visionSubsystem;
+    private GamepadSubsystem gamepadSubsystem;
+
+
+    private ShooterSubsystem shooterSubsystem;
+    private ChargeurSubsystem chargeurSubsystem;
+    private DrivePedroSubsystem drivePedroSubsystem;
+
+
+    /**
+     * Initialize the teleop OpMode, gamepads, buttons, and default commands.
+     */
+    @Override
+    public void initialize() {
+            super.initialize();
+
+            gamepadSubsystem = new GamepadSubsystem(gamepad1, gamepad2, telemetryM);
+
+            visionSubsystem = new VisionSubsystem(hardwareMap, telemetryM);
+            register(visionSubsystem);
+            visionSubsystem.setDefaultCommand(new ContinuousUpdatesAprilTagsDetections(visionSubsystem));
+
+            drivePedroSubsystem = new DrivePedroSubsystem(hardwareMap, BLUE_GOAL_POSE, telemetryM);
+            register(drivePedroSubsystem);
+
+
+            trieurSubsystem = new TrieurSubsystem(hardwareMap, telemetryM);
+            register(trieurSubsystem);
+
+            autoSetArtefactColors();
+
+            chargeurSubsystem = new ChargeurSubsystem(hardwareMap, telemetryM);
+            register(chargeurSubsystem);
+
+            shooterSubsystem = new ShooterSubsystem(hardwareMap, telemetryM);
+            register(shooterSubsystem);
+            shooterSubsystem.setDefaultCommand(new VisionShooter(shooterSubsystem, visionSubsystem));
+
+
+            new SequentialCommandGroup(
+                    // Obelisk and MoulinCalibrate
+                    new ParallelCommandGroup(
+                            new SequentialCommandGroup(
+                                    new MoulinCalibrationSequence(trieurSubsystem),
+                                    new ReadyMotif(trieurSubsystem, visionSubsystem, gamepadSubsystem)
+                            ),
+                            new SequentialCommandGroup(
+                                    new FollowPath(
+                                            drivePedroSubsystem, builder -> builder
+                                            .addPath(new BezierLine(
+                                                    BLUE_GOAL_POSE,
+                                                    CLOSE_SHOOT_BLUE_POSE)
+                                            ).setLinearHeadingInterpolation(BLUE_GOAL_POSE.getHeading(), CLOSE_SHOOT_BLUE_POSE.getHeading()).build(),
+                                            AUTO_ROBOT_CONSTRAINTS, true)
+                            )
+                    )
+//                    new ParallelCommandGroup(
+//                            new FollowPath(
+//                                drivePedroSubsystem, builder -> builder
+//                                .addPath(new BezierLine(
+//                                        OBELISK_BLUE_POSE,
+//                                        CLOSE_SHOOT_BLUE_POSE)
+//                            ).setLinearHeadingInterpolation(OBELISK_BLUE_POSE.getHeading(), CLOSE_SHOOT_BLUE_POSE.getHeading(), LINEAR_HEADING_INTERPOLATION_END_TIME).build(),
+//                            AUTO_ROBOT_CONSTRAINTS, false),
+//                            // Shoot All
+//                            new ShootRevolution(trieurSubsystem, shooterSubsystem, new InstantRangeVisionShooter(shooterSubsystem, visionSubsystem))
+//                    )
+
+
+                    // go to next row of artefacts
+//                    new ParallelRaceGroup(
+//                            new ModeRamassage(trieurSubsystem, shooterSubsystem, chargeurSubsystem, gamepadSubsystem),
+//                            new SequentialCommandGroup(
+//                                    new FollowPath(
+//                                            drivePedroSubsystem, builder -> builder
+//                                            .addPath(new BezierLine(
+//                                                    CLOSE_SHOOT_BLUE_POSE,
+//                                                    FIRST_ROW_BLUE_POSE)
+//                                            ).setLinearHeadingInterpolation(CLOSE_SHOOT_BLUE_POSE.getHeading(), FIRST_ROW_BLUE_POSE.getHeading()).build(),
+//                                            AUTO_ROBOT_CONSTRAINTS, true),
+//                                    new MaxPowerChargeur(chargeurSubsystem),
+//                                    new FollowPath(
+//                                            drivePedroSubsystem, builder -> builder
+//                                            .addPath(new BezierLine(
+//                                                    FIRST_ROW_BLUE_POSE,
+//                                                    FIRST_ROW_BLUE_POSE.withX(X_END_ROW_BLUE))
+//                                            ).setLinearHeadingInterpolation(FIRST_ROW_BLUE_POSE.getHeading(), FIRST_ROW_BLUE_POSE.getHeading()).build(),
+//                                            AUTO_ROBOT_CONSTRAINTS, true),
+//                                    new StopChargeur(chargeurSubsystem)
+//                            )
+//                    )
+            ).schedule();
+    }
+
+    /**
+     * Main OpMode loop. Updates gamepad states.
+     */
+    @Override
+    public void run() {
+            super.run();
+    }
+
+    /**
+     * auto set artefact colors
+     */
+    private void autoSetArtefactColors(){
+            trieurSubsystem.setMoulinStoragePositionColor(1, TrieurSubsystem.ArtifactColor.PURPLE);
+            trieurSubsystem.setMoulinStoragePositionColor(2, TrieurSubsystem.ArtifactColor.PURPLE);
+            trieurSubsystem.setMoulinStoragePositionColor(3, TrieurSubsystem.ArtifactColor.GREEN);
+    }
+
+}
