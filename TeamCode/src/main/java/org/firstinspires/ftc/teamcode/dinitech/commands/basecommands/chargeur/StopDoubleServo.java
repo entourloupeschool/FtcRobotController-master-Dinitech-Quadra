a2619862ecf303d6376a142b56a7a3111997b601
entourloupeschool <theo.dwk@free.fr>
2026-01-31 19:40:43 +0100

    feat: Add MaxPowerChargeur and MaxPowerDoubleServo commands for chargeur subsystem control refactor: Remove ReadyMotifAuto and MoulinVelocityControlled classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/basecommands/chargeur/StopDoubleServo.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.basecommands.chargeur;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.CHARGEUR_MOTOR_POWER;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ChargeurSubsystem;
+
+public class MaxPowerDoubleServo extends CommandBase {
+    private final ChargeurSubsystem chargeurSubsystem;
+
+    public MaxPowerDoubleServo(ChargeurSubsystem chargeurSubsystem){
+        this.chargeurSubsystem = chargeurSubsystem;
+        addRequirements(chargeurSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        chargeurSubsystem.setPowerDoubleServo(CHARGEUR_MOTOR_POWER);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
