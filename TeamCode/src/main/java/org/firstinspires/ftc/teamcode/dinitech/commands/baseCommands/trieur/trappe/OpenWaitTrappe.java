63c6862475697664cb1168a05f768728cdea5bcd
entourloupeschool <theo.dwk@free.fr>
2026-02-01 17:16:27 +0100

    feat: Add OpenWaitTrappe and WaitCloseTrappe command classes for trappe control feat: Implement FollowTrajectoryPedroTest opmode for trajectory testing feat: Create HubsSubsystem class for hub management

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/baseCommands/trieur/trappe/OpenWaitTrappe.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.trappe;
+
+public class OpenWaitTrappe {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/opmodes/tests/TestHubPatternColor.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.dinitech.opmodes.tests;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.AUTO_ROBOT_CONSTRAINTS;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.BEGIN_POSE;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.FollowPath;
+import org.firstinspires.ftc.teamcode.dinitech.opmodes.GornetixRobotBase;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.GamepadSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
+
+@Autonomous(name = "FollowTrajectoryPedroTest - Dinitech", group = "Test")
+public class FollowTrajectoryPedroTest extends GornetixRobotBase {
+    private GamepadSubsystem gamepadSubsystem;
+    private VisionSubsystem visionSubsystem;
+    private DrivePedroSubsystem drivePedroSubsystem;
+
+
+    /**
+     * Initialize the teleop OpMode, gamepads, buttons, and default commands.
+     */
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        gamepadSubsystem = new GamepadSubsystem(gamepad1, gamepad2, telemetryM);
+        register(gamepadSubsystem);
+
+        visionSubsystem = new VisionSubsystem(hardwareMap, telemetryM);
+        register(visionSubsystem);
+
+        drivePedroSubsystem = new DrivePedroSubsystem(hardwareMap, BEGIN_POSE, telemetryM);
+        register(drivePedroSubsystem);
+
+        new SequentialCommandGroup(
+                new FollowPath(drivePedroSubsystem,
+                        new PathChain(
+                                new Path(
+                                        new BezierLine(
+                                                BEGIN_POSE,
+                                                BEGIN_POSE.withX(BEGIN_POSE.getX() + 20)
+                                        )
+                                ),
+                                new Path(
+                                        new BezierLine(
+                                                drivePedroSubsystem.getPose(),
+                                                BEGIN_POSE
+                                        )
+                                )),
+                        AUTO_ROBOT_CONSTRAINTS, true),
+                new WaitCommand(1000),
+                new FollowPath(drivePedroSubsystem,
+                        new PathChain(
+                                new Path(
+                                        new BezierLine(
+                                                BEGIN_POSE,
+                                                BEGIN_POSE.withX(BEGIN_POSE.getX() + 20)
+                                        )
+                                ),
+                                new Path(
+                                        new BezierLine(
+                                                drivePedroSubsystem.getPose(),
+                                                BEGIN_POSE
+                                        )
+                                )),
+                        AUTO_ROBOT_CONSTRAINTS, true)
+        ).schedule();
+
+    }
+
+    /**
+     * Main OpMode loop. Updates gamepad states.
+     */
+    @Override
+    public void run() {
+            super.run();
+    }
+
+}
