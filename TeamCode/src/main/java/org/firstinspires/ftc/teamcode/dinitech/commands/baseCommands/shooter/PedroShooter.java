37cae216c54125176df563bf34788f011fd94ae5
entourloupeschool <theo.dwk@free.fr>
2026-02-04 00:10:08 +0100

    feat: Add VisionShooter command for dynamic shooter speed adjustment based on AprilTag range

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/baseCommands/shooter/PedroShooter.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.linearSpeedFromRange;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
+
+public class VisionShooter extends CommandBase {
+    private final ShooterSubsystem shooterSubsystem;
+    private final VisionSubsystem visionSubsystem;
+
+    /**
+     * Creates a VisionShooter command that adjusts shooter speed based on AprilTag
+     * range.
+     *
+     * @param shooterSubsystem The shooter subsystem
+     * @param visionSubsystem  The vision subsystem
+     */
+    public VisionShooter(ShooterSubsystem shooterSubsystem, VisionSubsystem visionSubsystem) {
+        this.shooterSubsystem = shooterSubsystem;
+        this.visionSubsystem = visionSubsystem;
+        addRequirements(shooterSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        shooterSubsystem.setUsageState(ShooterSubsystem.ShooterUsageState.VISION);
+    }
+
+
+    @Override
+    public void execute() {
+        Double range = visionSubsystem.getRangeToAprilTag();
+        if (range != null) shooterSubsystem.setVelocity(linearSpeedFromRange(range));
+    }
+
+
+}
