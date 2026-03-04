8eab6f68e8105a1d50c2e6fa977bd2367632fcda
entourloupeschool <theo.dwk@free.fr>
2026-03-04 22:13:12 +0100

    feat: Add ModeRamassageAuto command group for artifact collection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/modes/ModeRamassageAutoVintage.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.modes;
+
+
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.MoulinNextNextVeryLoose;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.PrepShootTrieur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.groups.ReadyTrieurForPick;
+import org.firstinspires.ftc.teamcode.dinitech.commands.groups.TryDetectArtefact;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.GamepadSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.TrieurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
+
+/**
+ * A command group that handles the artifact collection mode of the robot.
+ */
+public class ModeRamassageAuto extends SequentialCommandGroup {
+
+    /**
+     * Creates a new ModeRamassage command group.
+     *
+     * @param trieurSubsystem   The sorter subsystem, which manages artifact storage and state.
+     * @param gamepadSubsystem  The gamepad subsystem, passed down to child commands for haptic feedback.
+     */
+    public ModeRamassageAuto(TrieurSubsystem trieurSubsystem, VisionSubsystem visionSubsystem,
+                             GamepadSubsystem gamepadSubsystem) {
+        addCommands(
+                new ReadyTrieurForPick(trieurSubsystem),
+                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem),
+                new InstantCommand(() -> trieurSubsystem.etape = trieurSubsystem.etape + 1),
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new MoulinNextNextVeryLoose(trieurSubsystem),
+                                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem),
+                                new InstantCommand(() -> trieurSubsystem.etape = trieurSubsystem.etape + 1)),
+                        new InstantCommand(),
+                        trieurSubsystem::getNewRegister),
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new MoulinNextNextVeryLoose(trieurSubsystem),
+                                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem),
+                                new InstantCommand(() -> trieurSubsystem.etape = trieurSubsystem.etape + 1)),
+                        new InstantCommand(),
+                        trieurSubsystem::getNewRegister),
+                new InstantCommand(() -> trieurSubsystem.etape = trieurSubsystem.etape + 1),
+                new PrepShootTrieur(trieurSubsystem, visionSubsystem, gamepadSubsystem)
+        );
+    }
+}
