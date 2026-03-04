579f336270b929294ea20fb5ed95e8ccdec477a9
entourloupeschool <theo.dwk@free.fr>
2026-03-04 14:07:09 +0100

    refactor: Introduce ToRowToShoot command for enhanced shooting and path following

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/autoGroups/ToRowToShootChained.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.autoGroups;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.AUTO_ROBOT_CONSTRAINTS;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.LINEAR_HEADING_INTERPOLATION_END_TIME;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.MODE_RAMASSAGE_AUTO_TIMEOUT;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.MaxPowerChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.FollowPath;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter.SetVelocityShooterRequire;
+import org.firstinspires.ftc.teamcode.dinitech.commands.groups.ReadyTrieurForPick;
+import org.firstinspires.ftc.teamcode.dinitech.commands.groups.ShootHighSpeedIntel;
+import org.firstinspires.ftc.teamcode.dinitech.commands.modes.ModeRamassageAuto;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ChargeurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.GamepadSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.TrieurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
+
+public class ToRowToShoot extends SequentialCommandGroup {
+    public ToRowToShoot(DrivePedroSubsystem drivePedroSubsystem, TrieurSubsystem trieurSubsystem, ShooterSubsystem shooterSubsystem, ChargeurSubsystem chargeurSubsystem, VisionSubsystem visionSubsystem, GamepadSubsystem gamepadSubsystem, Pose RowPose, Pose endPose, double shooterVelocity, double lengthBackup, double rowPower, double endTime){
+        addCommands(
+                new ParallelCommandGroup(
+                        new ReadyTrieurForPick(trieurSubsystem),
+                        new FollowPath(drivePedroSubsystem, builder -> builder
+                                .addPath(new BezierLine(
+                                        drivePedroSubsystem::getPose,
+                                        RowPose))
+                                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(
+                                        drivePedroSubsystem::getHeading,
+                                        RowPose.getHeading(),
+                                        endTime)).build(),
+                                AUTO_ROBOT_CONSTRAINTS, true)),
+
+                new ParallelCommandGroup(
+                        new MaxPowerChargeur(chargeurSubsystem),
+                        new ModeRamassageAuto(trieurSubsystem, visionSubsystem, gamepadSubsystem, MODE_RAMASSAGE_AUTO_TIMEOUT),
+                        new SequentialCommandGroup(
+                                new FollowPath(drivePedroSubsystem, builder -> builder
+                                        .addPath(new BezierLine(
+                                                drivePedroSubsystem::getPose,
+                                                RowPose.withX(RowPose.getX() + (RowPose.getX() > 72 ? lengthBackup : -lengthBackup))))
+                                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(
+                                                drivePedroSubsystem::getHeading,
+                                                RowPose.getHeading(),
+                                                LINEAR_HEADING_INTERPOLATION_END_TIME)).build(),
+                                        rowPower, true),
+                                new SetVelocityShooterRequire(shooterSubsystem, shooterVelocity),
+                                // Go to Shooting Pos
+                                new FollowPath(drivePedroSubsystem, builder -> builder
+                                        .addPath(new BezierCurve(
+                                                drivePedroSubsystem::getPose,
+                                                RowPose,
+                                                endPose))
+                                        .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(
+                                                drivePedroSubsystem::getHeading,
+                                                endPose.getHeading(),
+                                                LINEAR_HEADING_INTERPOLATION_END_TIME)).build(),
+                                        AUTO_ROBOT_CONSTRAINTS, true))),
+
+                new ShootHighSpeedIntel(trieurSubsystem, shooterSubsystem)
+        );
+    }
+}
