59a2f77a35840a058e818a9d88842275dcc17a78
entourloupeschool <theo.dwk@free.fr>
2026-03-04 23:20:15 +0100

    feat: Add SetVelocityShooterRequire command with subsystem requirements

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/baseCommands/shooter/SetVelocityShooter.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.SPEED_MARGIN;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+
+public class SetVelocityShooterRequire extends CommandBase {
+    private final ShooterSubsystem shooterSubsystem;
+    protected final double ticksPerSecond;
+
+
+    public SetVelocityShooterRequire(ShooterSubsystem shooterSubsystem, double ticksPerSecond){
+        this.shooterSubsystem = shooterSubsystem;
+        this.ticksPerSecond = ticksPerSecond;
+        addRequirements(shooterSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        shooterSubsystem.setVelocity(ticksPerSecond);
+    }
+
+
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+
+}
