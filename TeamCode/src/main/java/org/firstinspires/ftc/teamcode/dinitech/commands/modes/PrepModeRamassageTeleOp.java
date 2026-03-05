da1a413ecb049c18bbb24ee125e9b58fced675e1
entourloupeschool <theo.dwk@free.fr>
2026-03-05 12:51:15 +0100

    refactor: Remove unused tapis commands and add new command groups for artifact collection and shooting modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/modes/PrepModeRamassageTeleOp.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.modes;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.StopChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.PedroAimLockedDrive;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter.PedroShooter;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ChargeurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.GamepadSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.HubsSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+
+/**
+ * A command group that handles the artifact collection mode of the robot.
+ */
+public class ModeShootTeleOp extends SequentialCommandGroup {
+
+    /**
+     * Creates a new ModeRamassage command group.
+     *
+     * @param drivePedroSubsystem    The drive subsystem for driving the robot.
+     * @param chargeurSubsystem The intake subsystem for running the intake motor.
+     * @param shooterSubsystem  The shooter subsystem for running the shooter motor.
+     * @param gamepadSubsystem  The gamepad subsystem, passed down to child commands for haptic feedback.
+     */
+    public ModeShootTeleOp(DrivePedroSubsystem drivePedroSubsystem, ShooterSubsystem shooterSubsystem, ChargeurSubsystem chargeurSubsystem, GamepadSubsystem gamepadSubsystem, HubsSubsystem hubsSubsystem) {
+        addCommands(
+            new StopChargeur(chargeurSubsystem),
+            new InstantCommand(()->shooterSubsystem.setDefaultCommand(new PedroShooter(shooterSubsystem, drivePedroSubsystem, hubsSubsystem)), shooterSubsystem),
+            new InstantCommand(()->drivePedroSubsystem.setDefaultCommand(new PedroAimLockedDrive(drivePedroSubsystem, gamepadSubsystem, hubsSubsystem)), drivePedroSubsystem)
+        );
+    }
+}
