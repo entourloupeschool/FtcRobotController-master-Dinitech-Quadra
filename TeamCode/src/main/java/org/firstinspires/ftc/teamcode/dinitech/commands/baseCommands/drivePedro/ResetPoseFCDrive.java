6926e302e9a6441ac3c22795b8db84898e389b7f
entourloupeschool <theo.dwk@free.fr>
2026-03-06 20:54:33 +0100

    feat: Add ResetPoseFCDrive and OnlyMotifDetections commands for improved robot navigation and vision processing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/baseCommands/drivePedro/ResetPoseFCDrive.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro;
+
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.HubsSubsystem;
+
+
+/**
+ * A command for reseting the heading of the robot.
+ *
+ */
+public class SetPoseFCDrive extends CommandBase {
+    private final DrivePedroSubsystem drivePedroSubsystem;
+    private final Pose inputPose;
+    /**
+     * Creates a new ResetHeadingFCDrive command.
+     *
+     * @param drivePedroSubsystem   The drive subsystem to control.
+     */
+    public SetPoseFCDrive(DrivePedroSubsystem drivePedroSubsystem, Pose inputPose) {
+        this.drivePedroSubsystem = drivePedroSubsystem;
+        this.inputPose = inputPose;
+        addRequirements(drivePedroSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        drivePedroSubsystem.getDrive().setPose(inputPose);
+        drivePedroSubsystem.setDriverInputPose(true);
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
