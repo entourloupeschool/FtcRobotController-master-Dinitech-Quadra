ebed261d7511138c5fa6d6c8511089acaae171cd
entourloupeschool <theo.dwk@free.fr>
2026-03-10 11:56:13 +0100

    feat: Add new artifact collection and shooting command groups for improved robot functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/groups/RamassageAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/groups/RamassageAuto.java
@@ -8,6 +8,7 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.InverseMaxPowerChargeur;
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.MaxPowerChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.StopChargeur;
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.MoulinNextEmptyStorage;
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.MoulinNextNextVeryLoose;
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.trieur.PrepShootTrieur;
@@ -21,7 +22,7 @@ import org.firstinspires.ftc.teamcode.dinitech.subsytems.VisionSubsystem;
 /**
  * A command group that handles the artifact collection mode of the robot.
  */
-public class ModeRamassageAutoGate extends SequentialCommandGroup {
+public class ModeRamassageAuto extends SequentialCommandGroup {
 
     /**
      * Creates a new ModeRamassage command group.
@@ -29,8 +30,29 @@ public class ModeRamassageAutoGate extends SequentialCommandGroup {
      * @param trieurSubsystem   The sorter subsystem, which manages artifact storage and state.
      * @param gamepadSubsystem  The gamepad subsystem, passed down to child commands for haptic feedback.
      */
-    public ModeRamassageAutoGate(TrieurSubsystem trieurSubsystem, VisionSubsystem visionSubsystem,
-                                 GamepadSubsystem gamepadSubsystem, ChargeurSubsystem chargeurSubsystem) {
+    public ModeRamassageAuto(TrieurSubsystem trieurSubsystem, VisionSubsystem visionSubsystem,
+                             GamepadSubsystem gamepadSubsystem) {
+        addCommands(
+                new TrieurReadyEmptyStorage(trieurSubsystem),
+                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem),
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new MoulinNextEmptyStorage(trieurSubsystem),
+                                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem)),
+                        new InstantCommand(),
+                        trieurSubsystem::getNewRegister),
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new MoulinNextEmptyStorage(trieurSubsystem),
+                                new TryDetectArtefact(trieurSubsystem, gamepadSubsystem)),
+                        new InstantCommand(),
+                        trieurSubsystem::getNewRegister),
+                new PrepShootTrieur(trieurSubsystem, visionSubsystem, gamepadSubsystem)
+        );
+    }
+
+    public ModeRamassageAuto(TrieurSubsystem trieurSubsystem, VisionSubsystem visionSubsystem,
+                             GamepadSubsystem gamepadSubsystem, ChargeurSubsystem chargeurSubsystem) {
         addCommands(
                 new ParallelCommandGroup(
                         new TrieurReadyEmptyStorage(trieurSubsystem),
@@ -49,9 +71,7 @@ public class ModeRamassageAutoGate extends SequentialCommandGroup {
                         new InstantCommand(),
                         trieurSubsystem::getNewRegister),
                 new ConditionalCommand(
-                        new ParallelCommandGroup(
-                                new MoulinNextEmptyStorage(trieurSubsystem),
-                                new InverseMaxPowerChargeur(chargeurSubsystem)),
+                        new InverseMaxPowerChargeur(chargeurSubsystem),
                         new InstantCommand(),
                         trieurSubsystem::getIsFull),
                 new PrepShootTrieur(trieurSubsystem, visionSubsystem, gamepadSubsystem)
