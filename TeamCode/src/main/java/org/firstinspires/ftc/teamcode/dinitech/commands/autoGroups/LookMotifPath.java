4c2c2d3584e09855ecdab24756ef52791d42e560
entourloupeschool <theo.dwk@free.fr>
2026-03-18 16:44:02 +0100

    feat: Add VoidEnd command for parallel execution of path following and subsystem stopping

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/autoGroups/LookMotifPath.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.autoGroups.endsequence;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.AUTO_ROBOT_CONSTRAINTS;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.LINEAR_HEADING_INTERPOLATION_END_TIME;
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.chargeur.StopChargeur;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.FollowPath;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter.StopShooter;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ChargeurSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+
+public class VoidEnd extends ParallelCommandGroup {
+    public VoidEnd(DrivePedroSubsystem drivePedroSubsystem, ShooterSubsystem shooterSubsystem, ChargeurSubsystem chargeurSubsystem, Pose voidPose){
+        addCommands(
+                new FollowPath(drivePedroSubsystem, builder -> builder
+                        .addPath(new BezierLine(
+                                drivePedroSubsystem::getPose,
+                                voidPose))
+                        .setLinearHeadingInterpolation(
+                                drivePedroSubsystem.getPose().getHeading(),
+                                voidPose.getHeading(),
+                                LINEAR_HEADING_INTERPOLATION_END_TIME).build(),
+                        AUTO_ROBOT_CONSTRAINTS, true),
+                new ParallelCommandGroup(
+                        new StopChargeur(chargeurSubsystem),
+                        new StopShooter(shooterSubsystem))
+        );
+    }
+}
