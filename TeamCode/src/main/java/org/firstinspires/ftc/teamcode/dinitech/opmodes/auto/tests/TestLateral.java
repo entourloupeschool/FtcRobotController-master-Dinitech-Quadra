7ca520f3eebcb7bc182dc5d5047f392a9585896b
entourloupeschool <theo.dwk@free.fr>
2026-03-20 15:38:00 +0100

    feat: Add TestPoseStorage autonomous OpMode for pose storage functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/opmodes/auto/tests/TestLateral.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.dinitech.opmodes.auto.tests;
+
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.FIELD_CENTER_90HEADING_POSE;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.dinitech.opmodes.auto.BlueGoalAutoBase;
+
+@Autonomous(name = "TestPoseStorage - Dinitech", group = "Test")
+//@Disabled
+public class TestPoseStorage extends BlueGoalAutoBase {
+
+    /**
+     * Initialize the teleop OpMode, gamepads, buttons, and default commands.
+     */
+    @Override
+    public void initialize() {
+            super.initialize();
+    }
+
+    /**
+     * Main OpMode loop. Updates gamepad states.
+     */
+    @Override
+    public void run() {
+            super.run();
+    }
+
+
+}
