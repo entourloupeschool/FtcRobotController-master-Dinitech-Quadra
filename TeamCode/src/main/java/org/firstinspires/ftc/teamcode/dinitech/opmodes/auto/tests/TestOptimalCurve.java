2e89cbe55a317dabe61712803fe9a1c363876bd6
entourloupeschool <theo.dwk@free.fr>
2026-03-25 15:17:48 +0100

    Refactor path commands: Remove OptimalPathV2.java and update TestOptimalCurve to use new OptimalPathV2 methods. Delete obsolete TestOptimalCurveV2 class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/opmodes/auto/tests/TestOptimalCurve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/opmodes/auto/tests/TestOptimalCurve.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.dinitech.opmodes.auto.tests;
 
 import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.AUTO_ROBOT_CONSTRAINTS;
 import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.CLOSE_SHOOT_RED_POSE;
+import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.FIELD_CENTER_90HEADING_POSE;
 import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.RESET_POSE_BLUE;
 import static org.firstinspires.ftc.teamcode.dinitech.other.Globals.RESET_POSE_RED;
 
@@ -10,26 +11,27 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.paths.OptimalPath;
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.drivePedro.paths.OptimalPathV2;
 import org.firstinspires.ftc.teamcode.dinitech.opmodes.auto.BlueTestResetPoseAutoBase;
 
-@Autonomous(name = "TestOptimalCurve - Dinitech", group = "Test")
+@Autonomous(name = "TestOptimalCurveV2 - Dinitech", group = "Test")
 //@Disabled
-public class TestOptimalCurve extends BlueTestResetPoseAutoBase {
+public class TestOptimalCurveV2 extends BlueTestResetPoseAutoBase {
 
     @Override
     public void initialize() {
             super.initialize();
 
             new SequentialCommandGroup(
-                    OptimalPath.triangleSpecificTangent(drivePedroSubsystem, CLOSE_SHOOT_RED_POSE, hubsSubsystem.getTeam().getCloseShootPose(), false, AUTO_ROBOT_CONSTRAINTS, true),
+                    OptimalPathV2.curve(drivePedroSubsystem, CLOSE_SHOOT_RED_POSE, hubsSubsystem.getTeam().getCloseShootPose(), AUTO_ROBOT_CONSTRAINTS, true),
                     new WaitCommand(1000),
-                    OptimalPath.triangleSpecificTangent(drivePedroSubsystem, CLOSE_SHOOT_RED_POSE, RESET_POSE_RED, true, AUTO_ROBOT_CONSTRAINTS, true),
+                    OptimalPathV2.curve(drivePedroSubsystem, FIELD_CENTER_90HEADING_POSE, RESET_POSE_RED, AUTO_ROBOT_CONSTRAINTS, true),
                     new WaitCommand(1000),
-                    OptimalPath.line(drivePedroSubsystem, hubsSubsystem.getTeam().getAudienceShootPose(),  AUTO_ROBOT_CONSTRAINTS, true),
+                    OptimalPathV2.line(drivePedroSubsystem, hubsSubsystem.getTeam().getAudienceShootPose(),  AUTO_ROBOT_CONSTRAINTS, true),
                     new WaitCommand(1000),
-                    OptimalPath.triangleSpecificTangent(drivePedroSubsystem, hubsSubsystem.getTeam().getCloseShootPose(), CLOSE_SHOOT_RED_POSE, false, AUTO_ROBOT_CONSTRAINTS, true),
+                    OptimalPathV2.curve(drivePedroSubsystem, hubsSubsystem.getTeam().getCloseShootPose(), CLOSE_SHOOT_RED_POSE, AUTO_ROBOT_CONSTRAINTS, true),
                     new WaitCommand(1000),
-                    OptimalPath.triangleSpecificTangent(drivePedroSubsystem, RESET_POSE_RED, RESET_POSE_BLUE, true, AUTO_ROBOT_CONSTRAINTS, true)
+                    OptimalPathV2.curve(drivePedroSubsystem, FIELD_CENTER_90HEADING_POSE, RESET_POSE_BLUE, AUTO_ROBOT_CONSTRAINTS, true)
             ).schedule();
     }
 
