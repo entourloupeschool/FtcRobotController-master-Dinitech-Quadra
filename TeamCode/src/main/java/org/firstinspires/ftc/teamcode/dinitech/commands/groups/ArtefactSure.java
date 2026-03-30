57451fda52d933b41b8269b886e50ef66193b89d
entourloupeschool <theo.dwk@free.fr>
2026-03-31 00:48:20 +0200

    feat: Remove InitToPedroShoot and InitToShoot classes; add WaitShoot command for improved shooting control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/commands/groups/ArtefactSure.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.dinitech.commands.groups;
+
+import static org.firstinspires.ftc.teamcode.dinitech.subsytems.TrieurSubsystem.WAIT_HIGH_SPEED_TRIEUR;
+
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelRaceGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
+
+import org.firstinspires.ftc.teamcode.dinitech.commands.baseCommands.shooter.MaxSpeedShooter;
+
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.dinitech.subsytems.TrieurSubsystem;
+
+
+public class WaitShoot extends SequentialCommandGroup {
+    public WaitShoot(ShooterSubsystem shooterSubsystem, TrieurSubsystem trieurSubsystem) {
+        addCommands(
+                new ParallelRaceGroup(
+                        new WaitUntilCommand(shooterSubsystem::isCurrentOverflow),
+                        new WaitCommand(WAIT_HIGH_SPEED_TRIEUR)),
+                new ConditionalCommand(
+                        new WaitCommand(WAIT_HIGH_SPEED_TRIEUR * 2L),
+                        new InstantCommand(),
+                        trieurSubsystem::wantsMotifShoot)
+        );
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/subsytems/devices/DinitechPredictiveFollower.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinitech/subsytems/devices/DinitechPredictiveFollower.java
@@ -1,49 +1,32 @@
-package org.firstinspires.ftc.teamcode.dinitech.subsytems.devices;
+package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import static org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem.ENCODER_RESOLUTION;
 import static org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem.PAR_POD_Y_MM;
 import static org.firstinspires.ftc.teamcode.dinitech.subsytems.DrivePedroSubsystem.PERP_POD_X_MM;
 
-import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.PredictiveBrakingCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-import com.bylazar.configurables.annotations.Configurable;
-@Configurable
-public class DinitechFollower {
-    public static final double AUTO_ROBOT_CONSTRAINTS = 1;
-    public static final double BRAKING_STRENGTH_PEDRO_DINITECH = 0.76; // 0.75
-    public static final double BRAKING_START_PEDRO_DINITECH = 1; // 1.4
-    public static double kF = 0.012;
+public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(12)
-            .forwardZeroPowerAcceleration(-52.3857)
-            .lateralZeroPowerAcceleration(-100.4664)
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0.005, 0.03, 0.012))
+            .predictiveBrakingCoefficients(new PredictiveBrakingCoefficients(0.1, 0.1153178675, 0.0022423725))
+            .centripetalScaling(0);
 
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.55, 0.007, 0.03, kF))
-            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.25, 0.0001, 0.0001, kF))
-            .useSecondaryTranslationalPIDF(false)
-
-            .headingPIDFCoefficients(new PIDFCoefficients(2, 0.005, 0.03, kF))
-            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(3, 0.06, 0.15, kF))
-            .useSecondaryHeadingPIDF(false)
-
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.11,0.001,0.012,0.3, kF))
-            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.0045,0.0003,0.0002,0.3, kF))
-            .useSecondaryDrivePIDF(false)
-
-            .centripetalScaling(0.0005);
-
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("rightFront")
@@ -56,7 +39,6 @@ public class DinitechFollower {
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .xVelocity(80.2)
             .yVelocity(67.35);
-
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(PAR_POD_Y_MM)  // https://pedropathing.com/docs/pathing/tuning/localization/pinpoint
             .strafePodX(PERP_POD_X_MM)
@@ -65,8 +47,6 @@ public class DinitechFollower {
             .customEncoderResolution(ENCODER_RESOLUTION) //through bore encoder https://www.revrobotics.com/rev-11-1271/ wheels: https://www.revrobotics.com/ION-Omni-Wheels/ // https://docs.revrobotics.com/rev-crossover-products/sensors/tbe/specs
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, BRAKING_STRENGTH_PEDRO_DINITECH, BRAKING_START_PEDRO_DINITECH);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
